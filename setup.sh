aafd934550e320b2a3daf8628a9475cba2f80a4d
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-11-07 19:51:44 -0500

    Added license prompt.
    Back to C-only for this repo.

--- setup.sh
+++ setup.sh
@@ -3,10 +3,6 @@
 PROJECT_OPTS=""
 while (( "$#" )); do
    case "$1" in
-      "flask")
-         PROJECT_OPTS="$PROJECT_OPTS -D do_flask=enabled"
-         ;;
-
       "gpl3")
          PROJECT_LICENSE="gpl3"
          ;;
@@ -18,8 +14,6 @@ while (( "$#" )); do
       *)
          if [ -z "$PROJECT_NAME" ]; then
             PROJECT_NAME="$1"
-         elif [ -z "$PROJECT_LANG" ]; then
-            PROJECT_LANG="$1"
          else
             echo "Unsupported options: $1"
             exit 16
@@ -37,38 +31,23 @@ if [ -z "$PROJECT_NAME" ]; then
    fi
 fi
 
-if [ -z "$PROJECT_LANG" ]; then
-   echo "Language? (c/python)"
-   read PROJECT_LANG
-   if [ -z "$PROJECT_LANG" ]; then
-      exit 4
+if [ -z "$PROJECT_LICENSE" ]; then
+   echo "License? (gpl3/lgpl3)"
+   read PROJECT_LICENSE
+   if [ -z "$PROJECT_LICENSE" ]; then
+      exit 32
    fi
 fi
 
 PROJECT_UPPER=`echo "$PROJECT_NAME" | \
    tr '[a-z]' '[A-Z]'`
 
-case $PROJECT_LANG in
-   "c")
-      TEMPLATE_FILES="
-         src/Makefile
-         src/template.c
-         src/template.h
-         tests/check.c
-         tests/check_template.c"
-      ;;
-   
-   "python")
-      TEMPLATE_FILES="
-         src/server.py
-         "
-      ;;
-
-   *)
-      echo "Invalid language specified."
-      exit 8
-      ;;
-esac
+TEMPLATE_FILES="
+   src/Makefile
+   src/template.c
+   src/template.h
+   tests/check.c
+   tests/check_template.c"
 
 if [ "$PROJECT_LICENSE" = "gpl3" ]; then
    wget "https://www.gnu.org/licenses/gpl-3.0.txt" -O LICENSE
